1daa749fe284dd2224eb9dfc5c00c11c5f73f81c
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-06-29 22:06:10 +0800

    Added good hack for viewing UI design of each template instead of relying on a stack.

--- mobile_food_app/app/_layout.jsx
+++ mobile_food_app/app/_layout.jsx
@@ -3,12 +3,15 @@ import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Slot } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { COLORS } from '../constants/colors'
+// import VerifyEmail from './(auth)/verify-email'
 
 export default function RootLayout() 
 {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
+        {/* Good hack for looking at a particular route for UI design */}
+        {/* <VerifyEmail />*/}
         <Slot />
       </SafeAreaView>
     </ClerkProvider>
