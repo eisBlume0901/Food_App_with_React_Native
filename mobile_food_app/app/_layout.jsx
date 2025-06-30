088a995c977752b6a93df6fcb72798098f4e21dd
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-06-30 20:29:59 +0800

    Resolved typo for tokenCahe to be tokenCache

--- mobile_food_app/app/_layout.jsx
+++ mobile_food_app/app/_layout.jsx
@@ -6,10 +6,11 @@ import { COLORS } from '../constants/colors'
 import SafeScreen from '../components/SafeScreen' // Use if you do not want home indicators to be seen
 // import VerifyEmail from './(auth)/verify-email'
 
+
 export default function RootLayout() 
 {
   return (
-    <ClerkProvider tokenCahe={tokenCache}>
+    <ClerkProvider tokenCache={tokenCache}>
       <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
         {/* Good hack for looking at a particular route for UI design */}
         {/* <VerifyEmail />*/}
