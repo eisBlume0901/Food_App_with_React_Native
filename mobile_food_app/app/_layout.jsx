270191ec81524a8b041936c01be510ddf348d4fb
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-06-30 19:35:59 +0800

    Added another good option instead of SafeAreaView used a component SafeScreen

--- mobile_food_app/app/_layout.jsx
+++ mobile_food_app/app/_layout.jsx
@@ -3,12 +3,13 @@ import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Slot } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { COLORS } from '../constants/colors'
+import SafeScreen from '../components/SafeScreen' // Use if you do not want home indicators to be seen
 // import VerifyEmail from './(auth)/verify-email'
 
 export default function RootLayout() 
 {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider tokenCahe={tokenCache}>
       <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
         {/* Good hack for looking at a particular route for UI design */}
         {/* <VerifyEmail />*/}
