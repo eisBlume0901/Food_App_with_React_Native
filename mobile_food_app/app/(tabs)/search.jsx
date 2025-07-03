326fda0b92e5635a601f33f275d41262d6aa8d81
eisBlume0901 <anecito2221737@mkt.ceu.edu.ph>
2025-07-03 22:07:11 +0800

    Added search ui and logic

--- mobile_food_app/app/(tabs)/search.jsx
+++ mobile_food_app/app/(tabs)/search.jsx
@@ -1,13 +1,127 @@
 import 
 { 
     View, 
-    Text 
+    Text, 
+    ScrollView,
+    TextInput
 } from 'react-native'
+import { MealAPI } from '../../services/mealAPI';
+import { searchStyles } from '../../assets/styles/search.styles';
+import { useState, useEffect } from 'react';
+import { useDebounce } from "../../hooks/useDebounce"
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { COLORS } from '../../constants/colors';
 
 const SearchScreen = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [initialLoading, setInitiaLoading] = useState(true);
+
+  const debouncedSearchQuery = useDebounce(searchQuery, 300); // Perform search only after 300 milliseconds
+
+  const performSearch = async (query) => {
+
+    // If there is no search query to be requested
+    if (!query.trim) {
+      const randomMeals = await MealAPI.getRandomMeals(12);
+      return randomMeals
+        .map( meal => MealAPI.transformMealData(meal) )
+        .filter(meal => meal !== null)
+    }
+
+    // First search by name, then by ingredient if no results
+    const nameResults = await MealAPI.searchMealsByName(query);
+    
+    let results = nameResults;
+
+    if (results.length === 0) {
+      const ingredientResults = await MealAPI.filterByIngredient(query);
+      results = ingredientResults;
+    }
+
+    return results
+      .slice(0, 12) // Get the top 12 results
+      .map(meal => MealAPI.transformMealData(meal)) // Since it is in JSON file, transform into a clean structured object
+      .filter(meal => meal !== null); // Filter out null values (so only with data is returned)  
+  }
+
+  // This effect is the default state when the user goes to Search Tab
+  useEffect(()=> {
+    const loadInitialData = async () => {
+      try
+      {
+        const results = await performSearch("");
+        setRecipes(results);
+      } 
+      catch(error)
+      {
+        console.error("Error loading initial data: ", error);
+      }
+      finally
+      {
+        setInitiaLoading(false);
+      }
+    }
+
+    loadInitialData();
+  }, [])
+
+  // This effect is when a user performs a search query
+  useEffect(() => {
+    if (initialLoading) {
+      return;
+    }
+
+    const handleSearch = async () => {
+      setLoading(true);
+
+      try 
+      {
+        const results = await performSearch(debouncedSearchQuery);
+
+        setRecipes(results);
+      } 
+      catch (error)
+      {
+        console.error("Error Searching: ", error);
+        setRecipes([]);
+      } 
+      finally 
+      {
+        setLoading(false);
+      }
+    };
+
+    handleSearch();
+  }, [debouncedSearchQuery, initialLoading]); // dependency array, every time a debouncedSearchQuery or initialLoading's state changes
+  // run the effect (it will not run if there is no changes)
+
+  if (initialLoading) {
+    return <Text>Loading some data...</Text>
+  }
+
   return (
-    <View>
-      <Text>search</Text>
+    <View style={searchStyles.container}>
+      <View style={searchStyles.searchSection}>
+        <View style={searchStyles.searchContainer}>
+          <Ionicons
+            name="search"
+            size={20}
+            color={COLORS.textLight}
+            style={searchStyles.searchItem}
+          />
+
+          <TextInput 
+            style={searchStyles.searchInput}
+            placeholder="Search recipes, ingredients..."
+            placeholderTextColor={COLORS.textLight}
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+            returnKeyType="search"
+          />
+        </View>
+      </View>
     </View>
   )
 }
